c2df878e158b21dbae527746ab272a1f5121e334
Alberto <94637799+meaar@users.noreply.github.com>
2023-02-05 22:59:00 -0300

    update filePath filter

--- FileCreate.java
+++ FileCreate.java
@@ -6,7 +6,23 @@ public class FileCreate {
     private File file;
 
     public FileCreate(String filePath) {
-
+        if (filePath.endsWith("/")) {
+            return;
+        }
+        this.fileName = filePath;
+        this.filePath = "";
+        if (filePath.contains("/")) {
+            String[] str = filePath.split("/");
+            if (str.length > 1) {
+                this.fileName = str[str.length-1];
+                for (int i = 0; i < str.length-1; i++) {
+                    if (this.filePath.endsWith("/") && str[i].isEmpty()) {
+                        return;
+                    }
+                    this.filePath = this.filePath+str[i]+"/";
+                }
+            }
+        }
     }
 
     public String getFilePath() {
